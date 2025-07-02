388a8e1f1f12cac8a21596cd5463fab33b374894
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-02 15:16:56 +0200

    Creazione dei DAO

--- src/main/java/maurotuzzolino/dao/EventoDAO.java
+++ src/main/java/maurotuzzolino/dao/EventoDAO.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityTransaction;
 import maurotuzzolino.entities.Evento;
 
 public class EventoDAO {
-
     private EntityManager entityManager;
 
     public EventoDAO(EntityManager entityManager) {
@@ -17,20 +16,10 @@ public class EventoDAO {
         transaction.begin();
         entityManager.persist(evento);
         transaction.commit();
-
-        System.out.println("L'evento " + evento.getTitolo() + " è stato aggiunto correttamente.");
+        System.out.println("Evento salvato: " + evento.getTitolo());
     }
 
     public Evento getById(long id) {
         return entityManager.find(Evento.class, id);
-    }
-
-    public void delete(Evento evento) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-        entityManager.remove(evento);
-        transaction.commit();
-
-        System.out.println("L'evento " + evento.getTitolo() + " è stato eliminato correttamente.");
     }
 }
